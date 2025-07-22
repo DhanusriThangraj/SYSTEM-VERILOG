8e8061803c5433d1ff9ba0cf1bf05398f50ac0e6
DHANUSRI THANGARAJ <146316804+DhanusriThangraj@users.noreply.github.com>
2025-07-22 18:36:48 +0530

    Create Eg_03.sv

---
+++ ARRAY/ASSOCIATIVE_ARRAY/Eg_03.sv
@@ -0,0 +1,27 @@
+odule associative_array;
+  int flowers[string];
+  
+  initial begin
+    $display("the initial value is ",flowers.size);
+    
+    flowers='{"rose":12,"jasmine":10,"lily":6,"tulip":46,"dallya":12};
+    $display("print the elements %p",flowers);
+    
+    $display("the values of rose %0d",flowers["rose"]);
+    $display("the values of jasmine %0d",flowers["jasmine"]);
+    $display("the values of tulip %0d",flowers["tulip"]);
+    $display("the values of dallya %0d",flowers["dallya"]);
+    
+  end
+endmodule
+
+// output
+xcelium> run
+the initial value is           0
+print the elements '{"dallya":12, "jasmine":10, "lily":6, "rose":12, "tulip":46}
+the values of rose 12
+the values of jasmine 10
+the values of tulip 46
+the values of dallya 12
+xmsim: *W,RNQUIE: Simulation is complete.
+xcelium> exit
