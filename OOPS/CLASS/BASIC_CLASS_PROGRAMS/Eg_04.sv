1aec54ee6f2d2a880166c6f2422144e0df2e3c98
DHANUSRI THANGARAJ <146316804+DhanusriThangraj@users.noreply.github.com>
2025-07-24 18:29:09 +0530

    Create Eg_04.sv

---
+++ OOPS/CLASS/BASIC_CLASS_PROGRAMS/Eg_04.sv
@@ -0,0 +1,27 @@
+class sample;
+  int x;
+  int y;
+  function void sum();
+    int result;
+    result=x+y;
+    $display("output of the a=%0d,b=%0d = sum=%0d",x,y,result);
+  endfunction
+endclass
+module sum;
+  sample s1=new();
+  initial begin
+    s1.x=12;
+    s1.y=121;
+    s1.sum;
+   
+    s1.x=12;
+    s1.y=11;
+    s1.sum;
+  end
+endmodule
+
+// output
+xcelium> run
+output of the a=12,b=121 = sum=133
+output of the a=12,b=11 = sum=23
+xmsim: *W,RNQUIE: Simulation is complete.
