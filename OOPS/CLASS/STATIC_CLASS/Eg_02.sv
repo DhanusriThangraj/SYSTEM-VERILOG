2352c7e5afd0624980b9304ec442c54c4d0b1efe
DHANUSRI THANGARAJ <146316804+DhanusriThangraj@users.noreply.github.com>
2025-07-25 17:13:37 +0530

    Create Eg_02.sv

---
+++ OOPS/CLASS/STATIC_CLASS/Eg_02.sv
@@ -0,0 +1,17 @@
+class example;
+  static int a=1;
+  static function void function_example();
+    $display("Display the values of a=%0d",a);
+  endfunction
+endclass
+module example_eg1;
+  initial begin
+    example::function_example();
+  end
+endmodule
+
+//output
+xcelium> run
+Display the values of a=1
+xmsim: *W,RNQUIE: Simulation is complete.
+xcelium> exit
