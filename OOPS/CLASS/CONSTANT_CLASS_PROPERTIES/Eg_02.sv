a529aaeb3285f314c4a93e2f30edb5806eaded80
DHANUSRI THANGARAJ <146316804+DhanusriThangraj@users.noreply.github.com>
2025-07-30 12:11:25 +0530

    Create Eg_02.sv

---
+++ OOPS/CLASS/CONSTANT_CLASS_PROPERTIES/Eg_02.sv
@@ -0,0 +1,20 @@
+class example;
+  const int x=1;
+   string y;
+  function new();
+    y="global instances"; 
+  endfunction
+  function void example1();
+    $display ("the values of a=%0d and y=%0s",x,y); 
+  endfunction
+endclass 
+
+module sample;
+  example eg1;
+  initial begin
+    eg1=new();
+  eg1.example1();
+    eg1.y="in global instances , the constant values are firstly assigned and it can be override in class and not overide in the module";
+    eg1.example1();
+  end
+endmodule
