1b29295b55014d7d8f33995f1391f4d409af25fb
DHANUSRI THANGARAJ <146316804+DhanusriThangraj@users.noreply.github.com>
2025-07-30 16:09:03 +0530

    Create Eg_01_with_this.sv

---
+++ OOPS/CLASS/INHERITANCE/Eg_01_with_this.sv
@@ -0,0 +1,25 @@
+class example;
+  int a=112;
+  int b=67;
+  function void display();
+    int a;
+    int b;
+    $display("Display the values of the a=%0d and b=%0d",a,b);
+    a=123;
+    b=678;
+    $display("Display the values of the a=%0d and b=%0d",a,b);
+    this.a=a;
+    this.b=b;
+    $display("Display the values of the a=%0d and b=%0d",a,b);
+    
+  endfunction
+endclass
+
+module sample;
+  example e1;
+  initial begin
+    e1=new;
+   e1.display();
+    $display("values of a=%0d b=%0d",e1.a,e1.b);
+  end
+endmodule
