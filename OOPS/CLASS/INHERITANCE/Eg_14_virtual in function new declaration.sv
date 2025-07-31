af42bf8a359e1fb706cf35fb6db8fdd6dbdc6fc2
DHANUSRI THANGARAJ <146316804+DhanusriThangraj@users.noreply.github.com>
2025-07-31 18:25:17 +0530

    Create Eg_14_virtual in function new declaration.sv

---
+++ OOPS/CLASS/INHERITANCE/Eg_14_virtual in function new declaration.sv
@@ -0,0 +1,26 @@
+ class example;
+  string a="dhanu";
+ virtual function new();
+    $display("a=%s",a);
+  endfunction
+endclass
+
+class example_1 extends example;
+  string b="qwerty";
+  function  new();
+    $display("b=%s",b);
+  endfunction
+  endclass
+
+module sample;
+  initial begin
+    example_1 e1;
+    e1=new();
+  end
+endmodule
+
+
+// output
+virtual function new();
+                    |
+xmvlog: *E,BADQAL (testbench.sv,208|20): Lifetime or qualifier(s) 'virtual' not allowed before function new declaration.
