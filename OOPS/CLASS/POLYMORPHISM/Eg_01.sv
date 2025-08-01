a84514edab48f6b94a6faadc2be0052c6e4d70bf
DHANUSRI THANGARAJ <146316804+DhanusriThangraj@users.noreply.github.com>
2025-08-01 13:08:02 +0530

    Create Eg_01.sv

---
+++ OOPS/CLASS/POLYMORPHISM/Eg_01.sv
@@ -0,0 +1,35 @@
+class example;
+  int a,b,c;
+  function void display();
+    a=78;
+    b=987;
+    c=99;
+    $display("display the values of the a=%0d,b=%0d,c=%0d",a,b,c);
+  endfunction
+endclass
+
+class example_1 extends example;
+  int a,b,c;
+  function void display();
+    a=78;
+    b=98;
+    c=a+b;
+    $display("values of a=%0d,b=%0d,c=%0d",a,b,c);
+  endfunction
+endclass
+
+module sample;
+  initial begin
+    example_1 e=new();
+    example r=e;
+    e.display();
+    r.display();
+  end
+endmodule
+
+// output
+xcelium> run
+values of a=78,b=98,c=176
+display the values of the a=78,b=987,c=99
+xmsim: *W,RNQUIE: Simulation is complete.
+xcelium> exit
