0041cfdfd67708b80ef26c497286865e162e3238
DHANUSRI THANGARAJ <146316804+DhanusriThangraj@users.noreply.github.com>
2025-08-01 18:45:22 +0530

    Create Eg_01_without_local.sv

---
+++ OOPS/CLASS/ENCAPSULATION/Eg_01_without_local.sv
@@ -0,0 +1,13 @@
+class example;
+  int a;
+endclass
+
+module sample;
+  initial begin
+    example s1=new();
+    s1.a=134;
+    $display("values of a=%0d",s1.a);
+  end
+endmodule
+// output
+Error-[SV-ICVA-L] Illegal class variable access
