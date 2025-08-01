3c119982ff257cb4f92d143aff82cf5c1c2bf0d2
DHANUSRI THANGARAJ <146316804+DhanusriThangraj@users.noreply.github.com>
2025-08-01 16:02:35 +0530

    Update Eg_06.sv

--- OOPS/CLASS/POLYMORPHISM/Eg_06.sv
+++ OOPS/CLASS/POLYMORPHISM/Eg_06.sv
@@ -17,8 +17,8 @@ module example;
   initial begin
     child_packet c1=new();
     packet p1;
-    p1=c1;
-    $cast(c1,p1);//dynamic casting
+    p1=c1;//up casting
+    $cast(c1,p1);//down casting
     j=c1.init();
     $display("j=%0d",j);
   end
@@ -29,3 +29,14 @@ xcelium> run
 j=-5
 xmsim: *W,RNQUIE: Simulation is complete.
 xcelium> exit
+
+// why we use $cast
+child_packet c1 = p1;  // ❌ Compile error!
+You must use $cast.
+
+| Operation   | Description                              | Allowed Without `$cast`? |
+| ----------- | ---------------------------------------- | ------------------------ |
+| Upcasting   | Derived → Base class (e.g., `c1` → `p1`) | ✅ Yes (implicit)         |
+| Downcasting | Base → Derived class (e.g., `p1` → `c1`) | ❌ No (requires `$cast`)  |
+
+
