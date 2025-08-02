cce9ce42fd76c27d44976fbfee30810dbb230a28
DHANUSRI THANGARAJ <146316804+DhanusriThangraj@users.noreply.github.com>
2025-08-02 14:29:56 +0530

    Create Eg_02.sv

---
+++ OOPS/CLASS/EXTERN/Eg_02.sv
@@ -0,0 +1,24 @@
+
+class example;
+  int a=123;
+  int b=987;
+  extern task  sum();    
+endclass
+task example::sum();
+      int sum;
+      sum=a+b;
+      $display("Display the values of sum=%0d",sum);
+endtask
+    
+    module sample();
+      initial begin
+        example e=new();
+        e.sum();
+      end
+    endmodule
+    
+// output
+xcelium> run
+Display the values of sum=1110
+xmsim: *W,RNQUIE: Simulation is complete.
+xcelium> exit
