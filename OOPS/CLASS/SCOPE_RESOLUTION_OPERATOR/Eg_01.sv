4ba6b43b62e8b1a9fb39d18a7918f3e53f4b1c3f
DHANUSRI THANGARAJ <146316804+DhanusriThangraj@users.noreply.github.com>
2025-08-02 15:34:03 +0530

    Update Eg_01.sv

--- OOPS/CLASS/SCOPE_RESOLUTION_OPERATOR/Eg_01.sv
+++ OOPS/CLASS/SCOPE_RESOLUTION_OPERATOR/Eg_01.sv
@@ -1,3 +1,7 @@
+// Acces to Static Properties and static methods from outside the class heirarchy
+Scope resolution operator in classes which is used to access the class properties and methods outside of the 
+ class using class name and followed by symbol :: and then the propeties and methods of a class.
+
 
 class example;
  static  string a="apple";
