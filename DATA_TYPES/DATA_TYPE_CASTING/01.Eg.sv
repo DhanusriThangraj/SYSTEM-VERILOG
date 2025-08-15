2e716bbe91f70a99defa9ce3d39086e1b0f1683b
DHANUSRI THANGARAJ <146316804+DhanusriThangraj@users.noreply.github.com>
2025-08-15 23:20:11 +0530

    Update 01.Eg.sv

--- DATA_TYPES/DATA_TYPE_CASTING/01.Eg.sv
+++ DATA_TYPES/DATA_TYPE_CASTING/01.Eg.sv
@@ -5,20 +5,21 @@ module integer_to_int;
   int int_data;
   initial begin
     integer_data=18'b11x11x1x11100zz1;
-    $display("size of the integer=%0d",$size(integer_data));
-    $display("display the values of the integer=%0b",integer_data);
+    $display("size of the integer=%d",$size(integer_data));
+    $display("display the values of the integer=%b",integer_data);
     
     int_data=int'(integer_data);
-    $display("size of the int=%0d",$size(int_data));
-    $display("After casting int=%0b",int_data);
+    $display("size of the int=%d",$size(int_data));
+    $display("After casting int=%b",int_data);
       
   end
 endmodule
 
 // output
 xcelium> run
-size of the integer=32
-display the values of the integer=11x11x1x11100zz1
-size of the int=32
-After casting int=1101101011100001
+size of the integer=         32
+display the values of the integer=000000000000000011x11x1x11100zz1
+size of the int=         32
+After casting int=00000000000000001101101011100001
 xmsim: *W,RNQUIE: Simulation is complete.
+xcelium> exit
