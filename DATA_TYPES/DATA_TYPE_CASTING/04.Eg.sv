905326f6c49d70b9c51a6ab6b6d8e8c8a0d7d93c
DHANUSRI THANGARAJ <146316804+DhanusriThangraj@users.noreply.github.com>
2025-08-16 00:03:00 +0530

    Update 04.Eg.sv

--- DATA_TYPES/DATA_TYPE_CASTING/04.Eg.sv
+++ DATA_TYPES/DATA_TYPE_CASTING/04.Eg.sv
@@ -2,18 +2,18 @@ module  logic_to_byte;
   logic logic_data;
   byte  byte_data;
   initial begin
-    logic_data='b1100xx11;   //when logic_data='b000xxxx; output will be x  and also logic_data='b000zz; output will be z
+    logic_data='bzz1100;  
     $display("logic_data=%b",logic_data);
     
-    byte_data=int'(logic_data);
+    byte_data=logic'(logic_data);
     $display("byte_data=%b",byte_data);
 
   end
 endmodule
-
 // output
 xcelium> run
-logic_data=1
-byte_data=00000001
+logic_data=0
+byte_data=00000000
 xmsim: *W,RNQUIE: Simulation is complete.
 xcelium> exit
+
