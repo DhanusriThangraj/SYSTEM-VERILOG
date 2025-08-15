880e38b93096b89e28325419077695cc684007d5
DHANUSRI THANGARAJ <146316804+DhanusriThangraj@users.noreply.github.com>
2025-08-15 22:53:30 +0530

    Create 02. 2_STATE.sv

---
+++ DATA_TYPES/STATES/02. 2_STATE.sv
@@ -0,0 +1,24 @@
+module state_2;
+  bit a;
+  byte b;
+  shortint c;
+  int d;
+  longint e;
+  initial begin
+    $display("(1)bit     =%b",a);
+    $display("(8)byte    =%b",b);
+    $display("(16)shortint=%b",c);
+    $display("(32)int     =%b",d);
+    $display("(64)longint=%b",e);
+  end
+endmodule
+
+// output
+xcelium> run
+(1)bit     =0
+(8)byte    =00000000
+(16)shortint=0000000000000000
+(32)int     =00000000000000000000000000000000
+(64)longint=0000000000000000000000000000000000000000000000000000000000000000
+xmsim: *W,RNQUIE: Simulation is complete.
+xcelium> exit
