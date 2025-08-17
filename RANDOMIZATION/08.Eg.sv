2a95da0434c1d1e41cf84feba7c793b172f033c6
DHANUSRI THANGARAJ <146316804+DhanusriThangraj@users.noreply.github.com>
2025-08-17 19:09:03 +0530

    Create 08.Eg.sv

---
+++ RANDOMIZATION/08.Eg.sv
@@ -0,0 +1,18 @@
+module example;
+  int data;
+  initial begin
+    repeat(4)begin
+      std::randomize(data);
+      $display("DATA=%0d",data);
+    end
+  end
+endmodule
+
+// output
+xcelium> run
+DATA=-182898798
+DATA=1997779049
+DATA=983378248
+DATA=605856704
+xmsim: *W,RNQUIE: Simulation is complete.
+xcelium> exit
