696abede2190835a3cc0b6df61129fb942cfba58
DHANUSRI THANGARAJ <146316804+DhanusriThangraj@users.noreply.github.com>
2025-08-30 14:23:01 +0530

    Update Eg_13.sv

--- LOOPS/Eg_13.sv
+++ LOOPS/Eg_13.sv
@@ -2,19 +2,17 @@ module eg;
   int a;
   initial begin
     while(a<10)begin
-    $display("A=%0d",a);
+      $display("A=%0d",a);
+      a++;
       if(a==3)begin
         break;
       end
-      a++;
     end
   end
 endmodule
-
 //output
 # run -all
 # A=0
 # A=1
 # A=2
-# A=3
 # exit
