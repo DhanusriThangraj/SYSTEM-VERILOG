3ed1eb4357ea44bb6ef745809df5227b4985a4db
DHANUSRI THANGARAJ <146316804+DhanusriThangraj@users.noreply.github.com>
2025-09-07 17:58:52 +0530

    Create 05.OUTPUT.sv

---
+++ INTERFACE/INTERFACE_EXAMPLE/01.AND_GATE/05.OUTPUT.sv
@@ -0,0 +1,11 @@
+Compiler version U-2023.03-SP2_Full64; Runtime version U-2023.03-SP2_Full64;  Sep  7 08:28 2025
+Time	A	B		OUT
+0	110	11111		110	
+10	11110	10011		10010	
+$finish called from file "top.sv", line 15.
+$finish at simulation time                   11
+           V C S   S i m u l a t i o n   R e p o r t 
+Time: 11 ns
+CPU Time:      0.360 seconds;       Data structure size:   0.0Mb
+Sun Sep  7 08:28:10 2025
+Done
