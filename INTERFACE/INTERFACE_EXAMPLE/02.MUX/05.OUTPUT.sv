a7e9a3e0ebee1c8e53e2c4b2bc7d3ea3f1812156
DHANUSRI THANGARAJ <146316804+DhanusriThangraj@users.noreply.github.com>
2025-09-07 17:57:23 +0530

    Create 05.OUTPUT.sv

---
+++ INTERFACE/INTERFACE_EXAMPLE/02.MUX/05.OUTPUT.sv
@@ -0,0 +1,13 @@
+xcelium> source /xcelium23.09/tools/xcelium/files/xmsimrc
+xcelium> run
+Time	A	sel	Y	
+0	x	x	x	
+1	1010	0	0	
+13	1010	1	1	
+28	1010	10	0	
+48	1010	11	1	
+Simulation complete via $finish(1) at time 49 NS + 0
+./top.sv:15     #1;$finish;
+xcelium> exit
+TOOL:	xrun	23.09-s001: Exiting on Sep 07, 2025 at 08:27:05 EDT  (total: 00:00:01)
+Done
