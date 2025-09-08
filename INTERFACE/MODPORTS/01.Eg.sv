b7dd713569a061367a8a92a50ffa073b91fa2c7f
DHANUSRI THANGARAJ <146316804+DhanusriThangraj@users.noreply.github.com>
2025-09-08 15:12:54 +0530

    Update 01.Eg.sv

--- INTERFACE/MODPORTS/01.Eg.sv
+++ INTERFACE/MODPORTS/01.Eg.sv
@@ -1,9 +1,15 @@
 interface inter;
   logic [3:0]a;
   logic [3:0]b;
-  logic [4:0]sum;
+  logic [3:0]c;
+  logic [4:0]sum,result;
   
   modport add( input a,b,
                output sum);
+  
+  modport sub (
+                 input sum,c,
+                 output result 
+            );
 endinterface;
 
