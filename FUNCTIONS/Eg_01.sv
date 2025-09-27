d1a46cbebfb325dc2d06159947ddd596aaad5886
DHANUSRI THANGARAJ <146316804+DhanusriThangraj@users.noreply.github.com>
2025-09-27 16:08:28 +0530

    Update Eg_01.sv

--- FUNCTIONS/Eg_01.sv
+++ FUNCTIONS/Eg_01.sv
@@ -3,14 +3,27 @@ module func;
   int a=2;
   int b=12;
   int variable_arguments;
+  int by_position;
   initial begin
     result = sum(2,9);//passing arguments by values
+    
     $display("passing arguments by values result=%0d",result);
-    $display("by using display (function)sum=%0d",sum(78,25)); //by using display;
+
+    $display("by using display (function)sum=%0d",sum(78,25)); //by using display
+    
+
     variable_arguments= sum(a,b);
     $display("passing arguments by variable_arguments=%0d",variable_arguments);
+    
+    
+     by_position = sum(.a(136),.b(678));
+    $display("passing arguments by positional=%0d",by_position);
+
   end
+    
   
+endmodule
+
   function int sum(int a,int b);
     int add;
     add=a+b;
@@ -18,10 +31,10 @@ module func;
       $display("add=%0d",add);
     return add;
   
-  endfunction 
-endmodule
+  endfunction
 
 // output
+Compiler version U-2023.03-SP2_Full64; Runtime version U-2023.03-SP2_Full64;  Sep 27 06:35 2025
 ---------------------
 add=11
 passing arguments by values result=11
@@ -31,8 +44,11 @@ by using display (function)sum=103
 ---------------------
 add=14
 passing arguments by variable_arguments=14
+---------------------
+add=814
+passing arguments by positional=814
            V C S   S i m u l a t i o n   R e p o r t 
 Time: 0 ns
-CPU Time:      0.440 seconds;       Data structure size:   0.0Mb
-Sat Sep 27 06:00:57 2025
+CPU Time:      0.370 seconds;       Data structure size:   0.0Mb
+Sat Sep 27 06:35:49 2025
 Done
