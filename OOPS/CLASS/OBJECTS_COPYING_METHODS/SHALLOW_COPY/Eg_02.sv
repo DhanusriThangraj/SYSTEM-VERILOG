7a07f79bb5e820400d1c705451cf3a876fa91c35
DHANUSRI THANGARAJ <146316804+DhanusriThangraj@users.noreply.github.com>
2025-10-02 21:50:05 +0530

    Update Eg_02.sv

--- OOPS/CLASS/OBJECTS_COPYING_METHODS/SHALLOW_COPY/Eg_02.sv
+++ OOPS/CLASS/OBJECTS_COPYING_METHODS/SHALLOW_COPY/Eg_02.sv
@@ -11,18 +11,26 @@ module sample;
   example d1,d2;
   initial begin
     d1 = new();
-    d1.display();
     d2=new d1;
+    d1.display();
+    d2.display();
+    $display("//////////////////////////////////////////");
     d2.a="DEVAKI";
+    d2.b=987;
     d1.display();
     d2.display();  
   end
 endmodule 
 
 // output
-xcelium> run
+Compiler version U-2023.03-SP2_Full64; Runtime version U-2023.03-SP2_Full64;  Oct  2 12:19 2025
 Display the values a=DHANUSRI and b=123423
 Display the values a=DHANUSRI and b=123423
-Display the values a=DEVAKI and b=123423
-xmsim: *W,RNQUIE: Simulation is complete.
-xcelium> exit
+//////////////////////////////////////////
+Display the values a=DHANUSRI and b=123423
+Display the values a=DEVAKI and b=987
+           V C S   S i m u l a t i o n   R e p o r t 
+Time: 0 ns
+CPU Time:      0.410 seconds;       Data structure size:   0.0Mb
+Thu Oct  2 12:19:29 2025
+Done
