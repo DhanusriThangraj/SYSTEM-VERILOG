5d24b08eb6a5e60504820d6a8f73261df09fed77
DHANUSRI THANGARAJ <146316804+DhanusriThangraj@users.noreply.github.com>
2025-10-02 21:46:47 +0530

    Update Eg_01.sv

--- OOPS/CLASS/OBJECTS_COPYING_METHODS/SHALLOW_COPY/Eg_01.sv
+++ OOPS/CLASS/OBJECTS_COPYING_METHODS/SHALLOW_COPY/Eg_01.sv
@@ -10,10 +10,15 @@ module sample;
   example eg1,eg2;
   initial begin
     eg1=new();
+    eg2= new eg1;
     eg1.display();
+    eg2.display();
+    $display("////////////////////////////////////////");
     eg1.a=145;
     eg1.display();
-    eg2= new eg1;   //shallow copy;
+    eg2.display();
+    $display("////////////////////////////////////////");
+    
     eg2.a=123;
     eg1.display();
     eg2.display();
@@ -21,13 +26,20 @@ module sample;
 endmodule
 
 // output
-xcelium> run
+Compiler version U-2023.03-SP2_Full64; Runtime version U-2023.03-SP2_Full64;  Oct  2 12:15 2025
 Display the values of a=10 and b=13
+Display the values of a=10 and b=13
+////////////////////////////////////////
 Display the values of a=145 and b=13
+Display the values of a=10 and b=13
+////////////////////////////////////////
 Display the values of a=145 and b=13
 Display the values of a=123 and b=13
-xmsim: *W,RNQUIE: Simulation is complete.
-xcelium> exit
+           V C S   S i m u l a t i o n   R e p o r t 
+Time: 0 ns
+CPU Time:      0.360 seconds;       Data structure size:   0.0Mb
+Thu Oct  2 12:15:48 2025
+Done
 
 
 
