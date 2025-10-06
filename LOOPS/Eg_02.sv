b35df1559a86f55411d71994f3bb43c37aea96bd
DHANUSRI THANGARAJ <146316804+DhanusriThangraj@users.noreply.github.com>
2025-10-06 10:52:43 +0530

    Update Eg_02.sv

--- LOOPS/Eg_02.sv
+++ LOOPS/Eg_02.sv
@@ -1,6 +1,6 @@
 
 module example;
-  int clk;
+  bit clk;
   initial begin
     while(1)begin
       #500 clk=~clk;
@@ -13,24 +13,30 @@ module example;
 endmodule
 
 // output
-# run -all
-# TIME=500 CLK=-1
-# TIME=1000 CLK=0
-# TIME=1500 CLK=-1
-# TIME=2000 CLK=0
-# TIME=2500 CLK=-1
-# TIME=3000 CLK=0
-# TIME=3500 CLK=-1
-# TIME=4000 CLK=0
-# TIME=4500 CLK=-1
-# TIME=5000 CLK=0
-# TIME=5500 CLK=-1
-# TIME=6000 CLK=0
-# TIME=6500 CLK=-1
-# TIME=7000 CLK=0
-# TIME=7500 CLK=-1
-# TIME=8000 CLK=0
-# TIME=8500 CLK=-1
-# TIME=9000 CLK=0
-# TIME=9500 CLK=-1
-# ** Note: $finish    : testbench.sv(105)
+Compiler version U-2023.03-SP2_Full64; Runtime version U-2023.03-SP2_Full64;  Oct  6 01:18 2025
+TIME=500 CLK=1
+TIME=1000 CLK=0
+TIME=1500 CLK=1
+TIME=2000 CLK=0
+TIME=2500 CLK=1
+TIME=3000 CLK=0
+TIME=3500 CLK=1
+TIME=4000 CLK=0
+TIME=4500 CLK=1
+TIME=5000 CLK=0
+TIME=5500 CLK=1
+TIME=6000 CLK=0
+TIME=6500 CLK=1
+TIME=7000 CLK=0
+TIME=7500 CLK=1
+TIME=8000 CLK=0
+TIME=8500 CLK=1
+TIME=9000 CLK=0
+TIME=9500 CLK=1
+$finish called from file "testbench.sv", line 75.
+$finish at simulation time                10000
+           V C S   S i m u l a t i o n   R e p o r t 
+Time: 10000 ns
+CPU Time:      0.510 seconds;       Data structure size:   0.0Mb
+Mon Oct  6 01:18:03 2025
+Done
