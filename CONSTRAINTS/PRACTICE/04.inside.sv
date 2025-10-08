33cdac7f720ef0e77dd68c913c49749bd38c249a
DHANUSRI THANGARAJ <146316804+DhanusriThangraj@users.noreply.github.com>
2025-10-08 11:58:10 +0530

    Update 04.inside.sv

--- CONSTRAINTS/PRACTICE/04.inside.sv
+++ CONSTRAINTS/PRACTICE/04.inside.sv
@@ -5,14 +5,19 @@ class example;
   rand bit [3:0]d;
   
   constraint c1 {a inside {[1:10]};};
-  constraint c2 {b inside {15,14};};
+  constraint c2 {!(b inside {15,14});};
   constraint c3 {c inside {1,3,[4:6]};};
   constraint c4 {d inside {[a:b]};};
+  
+  
 endclass
 
-module sample; 
+module sample;
+ 
   initial begin
   example e1;
+
+    
     e1=new();
 
     repeat(4) begin
@@ -22,14 +27,15 @@ end
    
   end
 endmodule
+
 //output
 xcelium> source /xcelium23.09/tools/xcelium/files/xmsimrc
 xcelium> run
-A=6-----B=15----C=5--------D=11
-A=4-----B=14----C=3--------D=6
-A=6-----B=14----C=4--------D=7
-A=8-----B=14----C=6--------D=10
+A=3-----B=8----C=1--------D=5
+A=8-----B=12----C=3--------D=10
+A=4-----B=12----C=6--------D=9
+A=10-----B=13----C=5--------D=12
 xmsim: *W,RNQUIE: Simulation is complete.
 xcelium> exit
-TOOL:	xrun	23.09-s001: Exiting on Oct 08, 2025 at 02:20:45 EDT  (total: 00:00:01)
+TOOL:	xrun	23.09-s001: Exiting on Oct 08, 2025 at 02:27:07 EDT  (total: 00:00:01)
 Done
